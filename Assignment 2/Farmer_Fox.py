777e9a0562bf806d03e82cb1b10e7e8d747b4e1e
Tianyuan Fu <ftygary@gmail.com>
2019-10-09 18:47:55 -0700

    [WIP] assignment 2 in progress

--- Assignment 2/Farmer_Fox.py
+++ Assignment 2/Farmer_Fox.py
@@ -1,16 +1,78 @@
 '''Farmer_Fox.py
-by John Nathan Smith
-UWNetID: jnsmith98
-Student number: 9876543
+by Tianyuan Fu
+UWNetID: tyfu
+Student number: 1974487
 
 Assignment 2, in CSE 415, Autumn 2019.
  
 This file contains my problem formulation for the problem of
 the Farmer, Fox, Chicken, and Grain.
+A farmer has to get his fox, chicken and grain across the river.
+The boat can only hold the farmer and one item.
+The fox cannot be left alone with the chicken.
+The chicken cannot be left alone with the grain.
 '''
+# <COMMON_DATA>
+# </COMMON_DATA>
 
-# Put your formulation of the Farmer-Fox-Chicken-and-Grain problem here.
-# Be sure your name, uwnetid, and 7-digit student number are given above in 
-# the format shown.
+# <COMMON_CODE>
+class State():
+    def __init__():
+    
 
-pass  # replace this.
+    def __eq__():
+
+    
+    def __str__():
+
+    
+    def __hash__():
+
+
+    def copy():
+
+
+    def can_move():
+    
+
+    def move():
+
+
+def goal_test():
+
+
+def goal_message():
+    
+
+class Operator:
+    def __init__():
+
+
+    def is_applicable():
+
+
+    def apply():
+
+# </COMMON_CODE>
+
+# <INITIAL_STATE>
+CREAT_INITIAL_STATE = 
+
+# </INITIAL_STATE>
+
+# <OPERATORS>
+# </OPERATORS>
+
+# <GOAL_TEST>
+# </GOAL_TEST>
+
+# <GOAL_MESSAGE_FUNCTION>
+# </GOAL_MESSAGE_FUNCTION>
+
+
+
+
+
+
+
+pass
